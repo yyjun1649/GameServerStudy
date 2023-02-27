89a16d8e93a2f6252a8bb5f6f630a559ac6708d6
yyjun1649 <yy164946@naver.com>
2023-02-27 21:36:31 +0900

    Init

---
+++ ServerCore/CorePch.h
@@ -0,0 +1,17 @@
+#pragma once
+
+#include "Types.h"
+#include <vector>
+#include <list>
+#include <queue>
+#include <stack>
+#include <map>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+#include <iostream>
+using namespace std;
+
+// GameServer과 DummyClient에서도 pch를 사용할 수 있도록 하는 클래스
+void HelloWorld();
